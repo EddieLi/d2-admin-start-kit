bf7a29757716250c76ed24bcce5061c0d42f7ce6
Eddie Li <lh.eddieli@gmail.com>
2019-12-11 23:30:03 -0500

    fixed translation issue

--- src/locales/mixin.js
+++ src/locales/mixin.js
@@ -5,8 +5,8 @@ export default {
       let message = `${this.$t('GENERAL.CURR_LANGUAGE')} ${this.$t('_name')} [ ${this.$i18n.locale} ]`
       if (['TRAVIS', 'NETLIFY'].includes(process.env.VUE_APP_BUILD_MODE)) {
         message = [
-          `当前语言：${this.$t('_name')} [ ${this.$i18n.locale} ]`,
-          `仅提供切换功能，没有配置具体的语言数据 `,
+          `${this.$t('GENERAL.CURR_LANGUAGE')} ${this.$t('_name')} [ ${this.$i18n.locale} ]`,
+          ``
         ].join('<br/>')
       }
       this.$notify({
